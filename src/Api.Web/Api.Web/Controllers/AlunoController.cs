3d9caa9b98be8cd6af065001d552179cb4288fa9
Ancelmo Luiz <44513782+AncelmoLuizHotmail@users.noreply.github.com>
2021-09-15 20:41:23 -0400

    Adicionar arquivos de projeto.

---
+++ src/Api.Web/Api.Web/Controllers/AlunoController.cs
@@ -0,0 +1,46 @@
+﻿using Api.Web.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunoController : ControllerBase
+    {
+        protected static IList<Aluno> _listaDeAlunos = new List<Aluno>();
+
+        [HttpPost]
+        public IActionResult Adicionar([FromBody] Aluno aluno)
+        {
+            var novoAluno = new Aluno { 
+                Id = aluno.Id,
+                Nome = aluno.Nome,
+                Idade = aluno.Idade,
+                Telefone = aluno.Telefone
+            };
+
+            _listaDeAlunos.Add(novoAluno);
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public IEnumerable<Aluno> Listar()
+        {
+            return _listaDeAlunos;
+        }
+
+        [HttpGet("{id}")]
+        public Aluno Buscar(int id)
+        {
+            var aluno = _listaDeAlunos.FirstOrDefault(x => x.Id == id);
+
+            return aluno;
+        }
+    }
+}
